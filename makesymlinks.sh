46fc041df4dc01c09cf89627b98ef0b15d1213ed
Jacob Young <jacob@jacobyoung.me>
2015-11-05 10:53:49 -0800

    Update makesymlinks name

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################
-# .make.sh
+# .makesymlinks.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
