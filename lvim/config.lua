d7cc4636e42ae74a515377a35c998d08e90b277a
Edward Wong Hau Pepelu Tivrusky IV <jacob.young@zoom.us>
2022-10-06 14:53:00 -0400

    Small changes to lvim startup to get dashboard

--- lvim/config.lua
+++ lvim/config.lua
@@ -195,8 +195,8 @@ lvim.builtin.telescope.defaults.sorting_strategy = "ascending"
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 
--- lvim.builtin.alpha.active = true
--- lvim.builtin.alpha.mode = 'dashboard'
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = 'dashboard'
 
 lvim.builtin.terminal.active = true
 
